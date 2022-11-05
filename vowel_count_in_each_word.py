dcef68365c23fb0e98e196d6e315edaa48a330ec
satish780 <84319683+satish780@users.noreply.github.com>
2022-11-05 10:29:54 +0530

    def v_cnt(a):     c=0     s="aeiou"     for i in a:         if i in s:             c+=1     return c n=input() m=n.lower() d=0 l=list(m.split(" ")) s=[] for i in l:     s.append(v_cnt(i)) print(*s)

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,15 @@
+def v_cnt(a):
+    c=0
+    s="aeiou"
+    for i in a:
+        if i in s:
+            c+=1
+    return c
+n=input()
+m=n.lower()
+d=0
+l=list(m.split(" "))
+s=[]
+for i in l:
+    s.append(v_cnt(i))
+print(*s)
